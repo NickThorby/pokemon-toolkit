a83949187468c2b0b23529043f164e0633bc972b
Nick <nick@exonic.co.za>
2023-05-12 21:00:25 +0200

    Added a missing init function for growth rate.

--- data/pokemon/growth_rate/pokemon_growth_rate.gd
+++ data/pokemon/growth_rate/pokemon_growth_rate.gd
@@ -4,5 +4,9 @@ class_name PokemonGrowthRate
 @export var name: String
 @export var exp_per_level: Array[int]
 
+func _init(p_name: String = "", p_exp_per_level: Array[int] = []):
+    self.name = p_name;
+    self.exp_per_level = p_exp_per_level;
+
 func get_exp_for_level(level: int) -> int:
     return exp_per_level[level-1];
