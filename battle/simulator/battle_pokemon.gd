5f1b5049ba114985a90ae63d84ab545184a025e5
Nick <nick@exonic.co.za>
2023-05-18 15:49:38 +0200

    Added pokemon methods

--- battle/simulator/battle_pokemon.gd
+++ battle/simulator/battle_pokemon.gd
@@ -1,2 +1,275 @@
 extends Object
 class_name BattlePokemon
+
+func _init():
+    pass
+
+func _get_moves():
+    pass
+
+func get_base_moves():
+    pass
+
+func get_slot():
+    pass
+
+func get_details():
+    pass
+
+func update_speed():
+    pass
+
+func calculate_stats():
+    pass
+
+func get_stat():
+    pass
+
+func get_action_speed():
+    pass
+
+func get_best_stat():
+    pass
+
+func get_weight():
+    pass
+
+func get_move_data():
+    pass
+
+func get_move_hit_data():
+    pass
+
+func allies_and_self():
+    pass
+
+func allies():
+    pass
+
+func adjacent_allies():
+    pass
+
+func foes():
+    pass
+
+func adjacent_foes():
+    pass
+
+func is_ally():
+    pass
+
+func is_adjacent():
+    pass
+
+func get_undynamaxed_hp():
+    pass
+
+func get_smart_targets():
+    pass
+
+func get_at_loc():
+    pass
+
+func get_loc_of():
+    pass
+
+func get_move_targets():
+    pass
+
+func ignoring_ability():
+    pass
+
+func ignoring_item():
+    pass
+
+func deduct_pp():
+    pass
+
+func move_used():
+    pass
+
+func got_attacked():
+    pass
+
+func get_last_attacked_by():
+    pass
+
+func get_last_damaged_by():
+    pass
+
+func get_locked_move():
+    pass
+
+func get_moves():
+    pass
+
+func max_move_disabled():
+    pass
+
+func get_dynamax_request():
+    pass
+
+func get_move_request_data():
+    pass 
+
+func get_switch_request_data():
+    pass
+
+func is_last_active():
+    pass
+
+func positive_boosts():
+    pass
+
+func get_capped_boost():
+    pass
+
+func boost_by():
+    pass
+
+func clear_boosts():
+    pass
+
+func set_boost():
+    pass
+
+func copy_volatile_from():
+    pass
+
+func transform_into():
+    pass
+
+func set_species():
+    pass
+
+func forme_change():
+    pass
+
+func clear_volatile():
+    pass
+
+func has_type():
+    pass
+
+func faint():
+    pass
+
+func damage():
+    pass
+
+func try_trap():
+    pass
+
+func has_move():
+    pass
+
+func disable_move():
+    pass
+
+func heal():
+    pass
+
+func set_hp():
+    pass
+
+func try_set_status():
+    pass
+
+func cure_status():
+    pass
+
+func set_status():
+    pass
+
+func clear_status():
+    pass
+
+func get_status():
+    pass
+
+func eat_item():
+    pass
+
+func use_item():
+    pass
+
+func take_item():
+    pass
+
+func set_item():
+    pass
+
+func get_item():
+    pass
+
+func has_item():
+    pass
+
+func clear_item():
+    pass
+
+func set_ability():
+    pass
+
+func get_ability():
+    pass
+
+func has_ability():
+    pass
+
+func clear_ability():
+    pass
+
+func get_nature(): 
+    pass
+
+func add_volatile():
+    pass
+
+func get_volatile(): 
+    pass
+
+func remove_volatile():
+    pass
+
+func remove_linked_volatiles():
+    pass
+
+func get_health():
+    pass
+
+func set_type():
+    pass
+
+func add_type():
+    pass
+
+func get_types():
+    pass
+
+func is_grounded():
+    pass
+
+func is_semi_invulnerable():
+    pass
+
+func is_sky_dropped():
+    pass
+
+func is_protected():
+    pass
+
+func effective_weather():
+    pass
+
+func run_effectiveness():
+    pass
+
+func run_immunity():
+    pass
+
+func run_status_immunity():
+    pass
+
+func destroy():
+    pass
